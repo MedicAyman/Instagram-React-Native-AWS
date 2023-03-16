0091eb78116646056e8682226aa9c864c968c886
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2023-03-16 17:56:43 +0200

    init, folder structure

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{color: colors.primary, fontSize: fonts.size.xlg}}>Hey world</Text>
+      <AntDesign name="stepforward" size={24} color="black" />
       <StatusBar style="auto" />
     </View>
   );
